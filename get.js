e27e97070848e84dab9c8aac9c9a540ea5d1f614
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-09-07 12:29:00 -0700

    pushing files on guthub

--- get.js
+++ get.js
@@ -4,15 +4,15 @@ var path = require("path");
 
 module.exports = function(app) {
   app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../tables.html"));
+    res.sendFile(path.join(__dirname, "./tables.html"));
   });
 
   app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../reserve.html"));
+    res.sendFile(path.join(__dirname, "./reserve.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../reserve.html"));
+    res.sendFile(path.join(__dirname, "./reserve.html"));
   });
 };
