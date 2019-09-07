7923841065e6baa3556e83f6c77ff41e270b7e5d
Ed Ritter <edritterre@gmail.com>
2019-09-07 12:31:11 -0700

    path changes

--- get.js
+++ get.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,7 +10,7 @@ app.use(express.json());
 //
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "tables.html"));
+  res.sendFile(path.join(__dirname, "view.html"));
 });
 
 app.get("/api/reservation", function(req, res) {
